c4cd46d7038ada2bdc8fcf7248037903d10fccd8
ashish-greycube <mr.ashish.shah@gmail.com>
2023-06-26 10:46:26 +0530

    chore: print from report basic

--- jammy_app/jammy_app/report/jammy__print_price_list/jammy__print_price_list.js
+++ jammy_app/jammy_app/report/jammy__print_price_list/jammy__print_price_list.js
@@ -11,5 +11,22 @@ frappe.query_reports["Jammy  Print Price List"] = {
 			"options":"Price List",
 		},
 
-	]
+	],
+	onload: (report) => {
+		// Create a button for setting the default supplier
+		report.page.add_inner_button(__("Print Price List"), () => {
+			if(!report.data) return;
+
+			let filters = report.get_values();
+			let price_list = filters.price_list || '';
+			console.log(price_list)
+			let url = `/api/method/jammy_app.jammy_app.report.jammy__print_price_list.jammy__print_price_list.get_print_pdf`;
+		// 	let filters = {
+		// 		filters
+		// };
+		console.log('filters',filters)
+		open_url_post(url, filters, true);				
+		});
+
+	},
 };
--- jammy_app/jammy_app/report/jammy__print_price_list/jammy__print_price_list.py
+++ jammy_app/jammy_app/report/jammy__print_price_list/jammy__print_price_list.py
@@ -4,97 +4,123 @@
 import frappe
 from frappe import _
 from frappe.core.utils import html2text
+from frappe.utils.pdf import get_pdf
 
 
 def execute(filters=None):
-	if not filters:
-		filters = {}
+    if not filters:
+        filters = {}
 
-	columns = get_columns(filters)
-	data = get_entries(filters)
-	return columns, data
+    columns = get_columns(filters)
+    data = get_entries(filters)
+    return columns, data
 
 def get_columns(filters):
-	columns = [
-		{
-			"label": _("ID"),
-			"fieldname": "id",
-			"fieldtype": "Link",
-			"options": "Item Price",
-			"width": 150,
-		},
-		{
-			"label": _("Price List"),
-			"fieldname": "price_list",
-			"fieldtype": "Link",
-			"options": "Price List",
-			"width": 170,
-		},
-		{
-			"label": _("Item Code"),
-			"fieldname": "item_code",
-			"fieldtype": "Data",
-			"options": "Item",
-			"width": 130,
-		},
-		{
-			"label": _("Item Name"),
-			"fieldname": "item_name",
-			"fieldtype": "Data",
-			"width": 200,
-		},
-		{
-			"label": _("Item Description"),
-			"fieldname": "description",
-			"fieldtype": "Data",
-			"width": 450,
-		},
-		{
-			"label": _("Rate"),
-			"fieldname": "rate",
-			"fieldtype": "Currency",
-			"width": 140,
-		},
-		{
-			"label": _("Pcs/Ctn"),
-			"fieldname": "pcs_ctn",
-			"fieldtype": "Int",
-			"width": 90,
-		},
-	]
-	return columns
+    columns = [
+        {
+            "label": _("ID"),
+            "fieldname": "id",
+            "fieldtype": "Link",
+            "options": "Item Price",
+            "width": 150,
+        },
+        {
+            "label": _("Price List"),
+            "fieldname": "price_list",
+            "fieldtype": "Link",
+            "options": "Price List",
+            "width": 170,
+        },
+        {
+            "label": _("Item Code"),
+            "fieldname": "item_code",
+            "fieldtype": "Data",
+            "options": "Item",
+            "width": 130,
+        },
+        {
+            "label": _("Item Name"),
+            "fieldname": "item_name",
+            "fieldtype": "Data",
+            "width": 200,
+        },
+        {
+            "label": _("Item Description"),
+            "fieldname": "description",
+            "fieldtype": "Data",
+            "width": 450,
+        },
+        {
+            "label": _("Rate"),
+            "fieldname": "rate",
+            "fieldtype": "Currency",
+            "width": 140,
+        },
+        {
+            "label": _("Pcs/Ctn"),
+            "fieldname": "pcs_ctn",
+            "fieldtype": "Int",
+            "width": 90,
+        },
+    ]
+    return columns
 
 def get_entries(filters):
-	conditions = get_conditions(filters)
+    conditions = get_conditions(filters)
 
-	entries = frappe.db.sql(
-		"""
-		SELECT
-			ip.name as id, ip.price_list as price_list, ip.item_code as item_code, ip.item_name as item_name,
-			ip.item_description as description, ip.price_list_rate as rate, i.pcs_ctn as pcs_ctn
-		FROM 
-			`tabItem Price` ip
-		INNER JOIN
-			`tabItem` i ON ip.item_code = i.item_code
-		WHERE
-			1=1
-			{conditions}
-		ORDER BY
+    entries = frappe.db.sql(
+        """
+        SELECT
+            ip.name as id, ip.price_list as price_list, ip.item_code as item_code, ip.item_name as item_name,
+            ip.item_description as description, ip.price_list_rate as rate, i.pcs_ctn as pcs_ctn
+        FROM 
+            `tabItem Price` ip
+        INNER JOIN
+            `tabItem` i ON ip.item_code = i.item_code
+        WHERE
+            1=1
+            {conditions}
+        ORDER BY
             ip.price_list,ip.item_code
 
-		""".format(conditions=conditions),filters,as_dict=1
-	)
-	for entry in entries:
-		
-		entry["description"] = html2text(entry["description"])
-		
-	return entries
-	
+        """.format(conditions=conditions),filters,as_dict=1
+    )
+    for entry in entries:
+        
+        entry["description"] = html2text(entry["description"])
+        
+    return entries
+    
 
 def get_conditions(filters):
-	
-	conditions =""
+    
+    conditions =""
 
-	if filters.get("price_list"):
-		conditions += " and ip.price_list = %(price_list)s"
-	return conditions
+    if filters.get("price_list"):
+        conditions += " and ip.price_list = %(price_list)s"
+    return conditions
+
+@frappe.whitelist()
+def get_print_pdf(price_list):
+    print('price_list',price_list)
+    filters={'price_list':price_list}
+    conditions = get_conditions(filters)
+    data=get_entries(filters)
+    options={}
+    args = {
+          "price_list": filters.get("price_list"),
+          "result":data
+     }    
+    html = frappe.get_template("jammy_app/jammy_app/report/jammy__print_price_list/jammy__print_price_list.html").render(args)
+    docname="Jammy Print Price List"
+    print(data)
+#      options={
+#         "page-width": "120mm",
+#         "page-height": "50mm",
+#         "margin-left":"17mm",
+#         "margin-right":"0mm"        
+# }
+    #  if print_type=='indirectpdf':
+    frappe.local.response.filename = "{name}.pdf".format(name=docname.replace(" ", "-").replace("/", "-"))
+    frappe.local.response.filecontent = get_pdf(html,options)
+    frappe.local.response.type = "pdf" 	
